28fad853234bfae2d1f0712d5979ed25b4f88038
i am a robot <adampluth@yahoo.com>
2016-09-02 10:17:51 -0500

    k

--- assign1.scm
+++ assign1.scm
@@ -44,7 +44,19 @@
         (define d 4)
         (define e 5)
         (inspect (min5 a b c d e))
-        )
+	(inpsect (min5 e d c b a))
+	(inpsect (min5 a e c d b))
+	(inpsect (min5 e c e b a))
+	)
+
+
+(define (run3)
+	(inspect(cym 0) )
+	(inspect(cym 25) )
+	(inspect(cym 50) )
+	(inspect(cym 75) )
+	(inspect(cym 100) )
+	)
 
 
 (define (min5 a b c d e)
@@ -57,4 +69,15 @@
 	(inspect(min2 a (min2 b (min2 c (min2 d e)))))
 	)
 
+
+
+(define(cym x)
+	(define c (cos x)
+	(define y (
+	)
+
+
+
 (run2)
+(run3)
+
