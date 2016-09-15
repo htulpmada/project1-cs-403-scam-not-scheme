df6df5f3c50227e3cc4cc9a7cd658ed5573bfe39
i am a robot <adampluth@yahoo.com>
2016-09-15 15:11:47 -0500

    3/4 egypt

--- assign1/assign1.scm
+++ assign1/assign1.scm
@@ -23,13 +23,12 @@
 ;(define (run7)
 ;	)
 
-;(define (run8)
-;	)
-
 (define (run8)
 	(egypt/ 1960 56)
 	)
 
+;(define (run9)
+;	)
 
 (define (run2)
         (inspect (min5 1 2 3 4 5))
@@ -163,10 +162,7 @@
 	(define a 1)
 	(define c num)
 	(define b div)
-	(define (double x) 
-		(define x2 (+ x x))
-	x2
-	)
+	(define (double x) (+ x x));need to define
 	(define (dblTil a2 b2 c2)
 	 (cond((> b2 c2) (define newvals (list a2 b2 c2)))
 	  (else (dblTil (double a2) (double b2) c2))
@@ -178,19 +174,26 @@
 	(define b (getElement newvals 1))
 	(define c (getElement newvals 2))
 	(define d 0)
+	(define (halfTil a b c d)
+	(
 
 
-)	
-(define (double x) 
-	(define x2 (+ x x))
-	x2
 	)
 
-(define (double2 x) 
-	(define x2 (+ x x))
-	(cond((= 0 x2)  1)
-	(else x2))
-	)
+
+)
+
+(define (double x)  
+  (define x2 (+ x x)) 
+  x2 
+  ) 
+
+(define (double2 x)  
+  (define x2 (+ x x)) 
+  (cond((= 0 x2)  1) 
+  (else x2)) 
+  ) 
+
 
 (define (halve x)
 	(define (recurHalf x y ts total)
@@ -200,23 +203,25 @@
 	 (inspect total)
 	  (cond 
 	    ((>(double2 x) y)
-;	    (inspect (>= 0 (- y x)))
+	    (inspect (>= 0 (- y x)))
 	    (cond 
-	      ((= 0 (- y x)) (inspect total))
-	      (else ;(inspect (- y x))
-	      ;(inspect (+ ts total))
-(recurHalf 1 (- y x) 0 (+ ts total))))
+	      ((>= 0 (- y x)) (cond ((= 0 total)1)
+				(else total )
+				))
+	      (else 
+		;(inspect (- y x))
+		;(inspect (+ ts total))
+		(recurHalf 1 (- y x) 0 (+ ts total))))
 	    )
-	  (else 
-	    (define ans(recurHalf (double x) y (double2 ts) total))))
-	  (recurHalf 1 y 0 (+ ts total))
-	  ans
-	   ; (else(recurHalf (double2 x) y (double2 ts)total))
+	  (else (inspect "repeat")
+	    (define ans(recurHalf (double x) y (double2 ts) total))))	
+;  (recurHalf 1 y 0 (+ ts total))
+;	  (inspect ans)
 	  )
 	(recurHalf 1 x 0 0)
 )
 
-(inspect(halve 4))
+(inspect (halve 9))
 
 ;(run1)
 ;(run2)
